f7dc30c2ffa81d837ac9dff51b061951f3d8adbb
Bill Bill <326874859@qq.com>
2018-12-04 23:11:35 +0800

    增加节点调试，增加批注信息

--- ex_2_4_7.c
+++ ex_2_4_7.c
@@ -3,6 +3,7 @@ int gcd(int x, int y)
 {
 	int r;
 	r = x % y;
+	//printf("x,y=%d,%d\n",x,y);
 	if (r == 0)
 		return y;
 	else
@@ -11,6 +12,8 @@ int gcd(int x, int y)
 int main()
 {
 	int a, b, m;
+	//对C语言中，需要注意输入时的分隔符号，否则容易引起结果错误
+	printf("请输入入参，并使用‘,’分隔a,b：");
 	scanf_s("%d,%d",&a,&b);
 	m = gcd(a, b);
 	printf("m=%d\n",m);
