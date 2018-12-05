e0b8d40b85c3ec8e74e37dc07de55fe3913bccd8
Bill <326874859@qq.com>
2018-12-05 14:52:57 +0800

    对基础习题碰到的问题进行注释解析

--- c_1_2.c
+++ c_1_2.c
@@ -3,8 +3,10 @@ int main()
 {
 	int a, b;
 	int sum(int x, int y);
+	//新的标准对scanf进行了调整，安全起见需使用scanf_s
 	scanf_s("%d,%d", &a, &b);
 	printf("The sum is %d\n",sum(a, b));
+	//需要增加系统暂停代码，否则连接生成后的程序一闪而过
 	system("pause");
 	return 0;
 }
