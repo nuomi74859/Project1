29d3b3b3d86677c305d8618118c8f851343bc3a2
Bill Bill <326874859@qq.com>
2019-01-03 23:20:37 +0800

    进行域宽、字符串 putchar getchar测试

--- c_3_5.c
+++ c_3_5.c
@@ -5,17 +5,18 @@ int main()
 {
 	float a,b,c,x1,x2,p;
 	printf("please input a,b,c\n");
-	scanf("%f,%f,%f",&a,&b,&c);
+	scanf_s("%f,%f,%f",&a,&b,&c);
 	p = b*b - 4*a*c;
 	if(p>=0)
 	{
 		x1 = (-b+sqrt(p))/(2*a);
 		x2 = (-b-sqrt(p))/(2*a);
-		printf("x1=%f,x2=%f\n",x1,x2 );
+		printf("x1=%7.3f,x2=%7.2f\n",x1,x2 );
 	}
 	else
 	{
 		printf("have no truce\n");
 	}
+	system("pause");
 	return 0;
 }
--- only_test.c
+++ only_test.c
@@ -5,8 +5,9 @@ int main()
 	int a, b;
 	a = 5;
 	b = 3;
-	b += ++a + b;
-	printf("a=%d,b=%d\n", a,b);
-	//system("pause");
+	b += ++a + b++ +10000000;
+	double c = 1.0;
+	printf("a=%10d\nb=%0d\nc=%10.1f%%\n%c\n", a,b,c/3,getchar());
+	system("pause");
 	return 0;
 }
