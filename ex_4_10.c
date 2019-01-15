4de12a5ed818a99250f329c884b0ab2d9720d832
Bill <326874859@qq.com>
2019-01-15 18:15:27 +0800

    增加switch方式进行计算 PS：switch只能接受整数类型

--- ex_4_10.c
+++ ex_4_10.c
@@ -1,10 +1,24 @@
 #include<stdio.h>
 int main()
 {
+	//float a = 0.1, b = 0.075, c = 0.05, d = 0.03, e = 0.015, f = 0.01;
+	float I;
+	float ifelse_bonus(float I), switch_bonus(float I);
+	do {
+		puts("请输入利润I：");
+		scanf_s("%f", &I);
+
+		printf("ifelse获得奖金总数为：%f\n", ifelse_bonus(I));
+		printf("switch获得奖金总数为：%f\n", switch_bonus(I));
+	} while (1);
+	system("pause");
+	return 0;
+}
+
+float ifelse_bonus(float I)
+{
+	float J;
 	float a = 0.1, b = 0.075, c = 0.05, d = 0.03, e = 0.015, f = 0.01;
-	float I, J;
-	puts("请输入利润I：");
-	scanf_s("%f", &I);
 	I /= 10000;
 	if (I <= 10)
 		J = a * I;
@@ -19,7 +33,30 @@ int main()
 	else
 		J = f * I + 2.95;
 	J *= 10000;
-	printf("获得奖金总数为：%f\n", J);
-	system("pause");
-	return 0;
+	return J;
+}
+
+float switch_bonus(float I)
+{
+	int i;
+	float J;
+	float a = 0.1, b = 0.075, c = 0.05, d = 0.03, e = 0.015, f = 0.01;
+	I /= 10000;
+	i = I / 10;
+	switch (i)
+	{
+		case 0:J = a * I; break;
+		case 1:J = b * I + 0.25; break;
+		case 2:
+		case 3:J = c * I + 0.75; break;
+		case 4:
+		case 5:J = d * I + 1.55; break;
+		case 6:
+		case 7:
+		case 8:
+		case 9:J = e * I + 2.45; break;
+		default:J = f * I + 2.95;
+	}	
+	J *= 10000;
+	return J;
 }
